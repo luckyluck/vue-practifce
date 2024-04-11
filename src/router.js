6f3a2c75ac7d0fd67d26ffb7d7cb9e314e5aae75
Alexandr Ketov <alexandr.ketov@platform24.com>
2024-04-11 08:28:49 +0200

    feat: make soma pages async

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachesList from '@/pages/coaches/CoachesList.vue';
-import CoachDetails from '@/pages/coaches/CoachDetails.vue';
-import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
-import RequestsReceived from '@/pages/requests/RequestsReceived.vue';
-import ContactCoach from '@/pages/requests/ContactCoach.vue';
 import NotFound from '@/pages/NotFound.vue';
-import UserAuth from '@/pages/auth/UserAuth.vue';
 import store from './store';
 
+const ContactCoach = () => import('@/pages/requests/ContactCoach.vue');
+const CoachDetails = () => import('@/pages/coaches/CoachDetails.vue');
+const CoachRegistration = () => import('@/pages/coaches/CoachRegistration.vue');
+const RequestsReceived = () => import('@/pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('@/pages/auth/UserAuth.vue');
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -18,15 +18,21 @@ const router = createRouter({
       path: '/coaches/:id',
       component: CoachDetails,
       props: true,
-      children: [
-        { path: 'contact', component: ContactCoach }
-      ]
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
-    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
     { path: '/auth', component: UserAuth, meta: { requiresAnonymous: true } },
     { path: '/:notFound(.*)', component: NotFound },
-  ]
+  ],
 });
 
 router.beforeEach((to, _, next) => {
