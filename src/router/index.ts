3e6a13794654353324f247e42fc0aee2f4f4a734
Alexandr Ketov <alexandr.ketov@platform24.com>
2024-04-14 13:39:38 +0200

    fix: usage of pinia in the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // @ts-ignore
 import CoachesList from '@/pages/coaches/CoachesList.vue'
 import NotFound from '@/pages/NotFound.vue'
-// import { useAuthStore } from '@/stores'
+import { useAuthStore } from '@/stores'
 
 // @ts-ignore
 const ContactCoach = () => import('@/pages/requests/ContactCoach.vue')
@@ -15,8 +15,6 @@ const CoachRegistration = () => import('@/pages/coaches/CoachRegistration.vue')
 const RequestsReceived = () => import('@/pages/requests/RequestsReceived.vue')
 // @ts-ignore
 const UserAuth = () => import('@/pages/auth/UserAuth.vue')
-
-// const authStore = useAuthStore()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,15 +42,16 @@ const router = createRouter({
   ]
 })
 
-// FIXME
-// router.beforeEach((to, _, next) => {
-//   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-//     next('/auth')
-//   } else if (to.meta.requiresAnonymous && authStore.isAuthenticated) {
-//     next('/coaches')
-//   } else {
-//     next()
-//   }
-// })
+router.beforeEach((to, _, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/auth')
+  } else if (to.meta.requiresAnonymous && authStore.isAuthenticated) {
+    next('/coaches')
+  } else {
+    next()
+  }
+})
 
 export default router
